5d57e260d2f1b00f5f221e3cd5c737390feefc60
A-A-Tyurin <avt2@yandex.ru>
2022-06-23 21:50:24 +0300

    v.1 - update routes

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,7 @@
 ''' API endpoints. '''
 
+from http import HTTPStatus
+
 from flask import request, jsonify, make_response
 from flask import current_app as app
 from pydantic import ValidationError
@@ -11,21 +13,23 @@ from .schemas import WishItemSchema
 
 @app.route('/', methods=['GET', 'POST'])
 def wishlist():
-    if request.method == 'GET':
-        # Get list of wishitems.
-        db_items = WishItem.query.all()
-        response = [WishItemSchema.from_orm(db_item).dict() for db_item in db_items]
-        status = 200
-    elif request.method == 'POST':
+    if request.method == 'POST':
         # Save new wishitem.
         try:
             item = WishItemSchema.parse_raw(request.data)
             db_new_item = WishItem(**item.dict()).create()
             response = WishItemSchema.from_orm(db_new_item).dict()
-            status = 201
+            status = HTTPStatus.CREATED
         except (ValidationError, IntegrityError) as error:
             response = str(error)
-            status = 400        
+            status = HTTPStatus.BAD_REQUEST
+    else:
+        # Get list of wishitems.
+        db_items = WishItem.query.all()
+        response = [
+            WishItemSchema.from_orm(db_item).dict() for db_item in db_items
+        ]
+        status = HTTPStatus.OK       
     return make_response(jsonify(response), status)
 
 
@@ -33,24 +37,27 @@ def wishlist():
 def wishitem(id):
     db_item = WishItem.query.get(id)
     if db_item is None:
-        return make_response(jsonify('Item not found'), 404)
-    if request.method == 'GET':
-        # Get wishitem by id.
-        response = WishItemSchema.from_orm(db_item).dict()
-        status = 200
-    elif request.method == 'DELETE':
+        return make_response(
+            jsonify(HTTPStatus.NOT_FOUND.phrase), HTTPStatus.NOT_FOUND
+        )
+
+    if request.method == 'DELETE':
         # Delete wishitem by id.
         db_item.delete()
         response = ''
-        status = 204
+        status = HTTPStatus.NO_CONTENT
     elif request.method == 'PUT':
         # Full update wishitem by id.
         try:
             item = WishItemSchema.parse_raw(request.data)
             updated_item = db_item.update(**item.dict())
             response = WishItemSchema.from_orm(updated_item).dict()
-            status = 200
+            status = HTTPStatus.OK
         except ValidationError as error:
             response = str(error)
-            status = 400
+            status = HTTPStatus.BAD_REQUEST
+    else:
+        # Get wishitem by id.
+        response = WishItemSchema.from_orm(db_item).dict()
+        status = HTTPStatus.OK
     return make_response(jsonify(response), status)
